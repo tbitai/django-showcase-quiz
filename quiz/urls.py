1dbc007942dce420d84346df28b80ad2f658bd73
Tamás Bitai <bitai.tamas@gmail.com>
2020-05-17 13:55:54 +0200

    Choice form action and CSRF token; empty check view

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'quiz'
 urlpatterns = [
     path('', views.first),
+    path('<int:question_id>/check/', views.check, name='check'),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -6,3 +6,7 @@ def first(request):
     first_question = Question.objects.earliest('id')
     context = {'first_question': first_question}
     return render(request, 'quiz/first.html', context)
+
+def check(request):
+    pass
+    
