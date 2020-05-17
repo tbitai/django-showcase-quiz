f49eb42c2f9624f9d3dbc6026ecea6365b328f4e
Tamás Bitai <bitai.tamas@gmail.com>
2020-05-17 12:18:03 +0200

    HTML template for first question

--- quiz/views.py
+++ quiz/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 def first(request):
     first_question = Question.objects.earliest('id')
-    return HttpResponse(first_question.text)
+    template = loader.get_template('quiz/first.html')
+    context = {'first_question': first_question}
+    return HttpResponse(template.render(context, request))
