667edc21eff974a237b49c15edba065ea41b4568
Tamás Bitai <bitai.tamas@gmail.com>
2020-05-17 12:23:43 +0200

    Refactor template rendering with render shortcut

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
 def first(request):
     first_question = Question.objects.earliest('id')
-    template = loader.get_template('quiz/first.html')
     context = {'first_question': first_question}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'quiz/first.html', context)
