148cb75f3b9c21292e09c4705216ef140334a0ad
Tamás Bitai <bitai.tamas@gmail.com>
2020-05-17 14:10:48 +0200

    Check view returning True/False

--- quiz/views.py
+++ quiz/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from .models import Question
+from .models import Question, Choice
 
 def first(request):
     first_question = Question.objects.earliest('id')
     context = {'first_question': first_question}
     return render(request, 'quiz/first.html', context)
 
-def check(request):
-    pass
+def check(request, question_id):
+    choice = Choice.objects.get(id=request.POST['choice'])
+    return HttpResponse(choice.correct)
     
